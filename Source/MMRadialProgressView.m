b8370fd4fc45d86cbefafba30a37ef45da9048f4
Lars Anderson <lars.anderson@mutualmobile.com>
2013-06-19 10:49:51 -0500

    Made radial progress look better. organized radial drawRect code

--- Source/MMRadialProgressView.m
+++ Source/MMRadialProgressView.m
@@ -34,50 +34,81 @@
 }
 
 - (void)drawInContext:(CGContextRef)ctx{
-    CGFloat insetWidth = 1.f;
+    CGFloat insetWidth = 1.f/[[UIScreen mainScreen] scale];
     CGFloat radiusOffset = (self.contentsScale > 1) ? 0.5f : 0.f;
-    CGRect rect = CGRectIntegral(self.bounds);
+    CGRect rect = CGRectInset(CGRectIntegral(self.bounds), 1.f, 1.f);
     CGRect insetRect = CGRectInset(rect, insetWidth, insetWidth);
     
-    CGFloat radius = truncf(MIN(CGRectGetWidth(insetRect)/2, CGRectGetHeight(insetRect)/2)/* - insetWidth*2*/) + radiusOffset;
-    CGPoint center = CGPointMake(CGRectGetWidth(insetRect)/2 + insetWidth, CGRectGetHeight(insetRect)/2 + insetWidth);
-    CGFloat topAngle = -(M_PI/2);
+    CGFloat radius = truncf(MIN(CGRectGetMidX(insetRect), CGRectGetMidY(insetRect))) + radiusOffset;
+    CGPoint center = CGPointMake(CGRectGetMidX(insetRect), CGRectGetMidY(insetRect));
+    CGFloat topAngle = -(M_PI_2);
     CGFloat endAngle = topAngle + (2*M_PI) * self.progress;
+    UIColor *backgroundFill = [UIColor colorWithWhite:1.0 alpha:0.2];
+    UIColor *pieFillColor = [UIColor whiteColor];
+    UIColor *borderStroke = [UIColor colorWithWhite:1.f alpha:0.5f];
     
-    //this is necessary since we're not calling this code in drawRect: in order to use UIKit draw methods.
-    // UIKit draw methods draw into the context at the top of the context stack, which in the case
-    // of this layer, is nil. Manually push a CGContextRef to the stack to draw on it.
+    /** This is necessary since we're not calling this
+        code in drawRect: in order to use UIKit draw methods.
+        UIKit draw methods draw into the context at the top
+        of the context stack, which in the case of this layer,
+        is nil. Manually push a CGContextRef to the stack to
+        draw on it.
+     */
     UIGraphicsPushContext(ctx);
     
-    //create path for background
-    UIBezierPath *circlePath = [UIBezierPath bezierPathWithOvalInRect:insetRect];
+    CGRect innerCircleRect = CGRectInset(rect,
+                                         CGRectGetWidth(insetRect)/5,
+                                         CGRectGetHeight(insetRect)/5);
     
-    //fill background
-    [[UIColor colorWithWhite:1.0 alpha:0.2] setFill];
-    [circlePath fill];
-    
-    //path for pie piece
-    UIBezierPath *piePath = [UIBezierPath bezierPath];
-    [piePath moveToPoint:center];
-    [piePath addArcWithCenter:center radius:radius startAngle:topAngle endAngle:endAngle clockwise:YES];
-    [piePath closePath];
-    
-    //fill pie piece
-    [[UIColor whiteColor] setFill];
-    [piePath fill];
-    
-    //stroke border
-    [[UIColor whiteColor] setStroke];
-    [circlePath stroke];
+    CGContextSaveGState(ctx);
+    {
+        //create path for background
+        UIBezierPath *circlePath = [UIBezierPath bezierPathWithOvalInRect:insetRect];
+        UIBezierPath *innerCirclePath = [UIBezierPath bezierPathWithOvalInRect:innerCircleRect];
+        
+        [circlePath appendPath:innerCirclePath];
+        [circlePath setUsesEvenOddFillRule:YES];
+        [circlePath addClip];
+        
+        //fill background
+        [backgroundFill setFill];
+        [circlePath fill];
+        
+        //path for pie piece
+        UIBezierPath *piePath = [UIBezierPath bezierPath];
+        [piePath moveToPoint:center];
+        [piePath addArcWithCenter:center
+                           radius:radius
+                       startAngle:topAngle
+                         endAngle:endAngle
+                        clockwise:YES];
+        [piePath closePath];
+        
+        //fill pie piece
+        [pieFillColor setFill];
+        [piePath fill];
+        
+        //stroke border
+        [borderStroke setStroke];
+        [circlePath stroke];
+        [innerCirclePath stroke];
+    }
+    CGContextRestoreGState(ctx);
     
     //stroke clear border to antialias
-    UIBezierPath *outerCirclePath = [UIBezierPath bezierPathWithOvalInRect:CGRectInset(rect, insetWidth/2, insetWidth/2)];
+    CGRect outerAntialiasRect = CGRectInset(insetRect, -insetWidth/2, -insetWidth/2);
+    CGRect innerAntialiasRect = CGRectInset(innerCircleRect, insetWidth/2, insetWidth/2);
+    UIBezierPath *outerCirclePath = [UIBezierPath bezierPathWithOvalInRect:outerAntialiasRect];
+    UIBezierPath *innerCircleAntialiasPath = [UIBezierPath bezierPathWithOvalInRect:innerAntialiasRect];
+    [outerCirclePath appendPath:innerCircleAntialiasPath];
+    
     [[UIColor clearColor] setStroke];
+    [innerCircleAntialiasPath setLineWidth:insetWidth];
+    [outerCirclePath setLineWidth:insetWidth];
     [outerCirclePath stroke];
+    [innerCircleAntialiasPath stroke];
     
     UIGraphicsPopContext();
-    
-    [super drawInContext:ctx];
 }
 @end
 
