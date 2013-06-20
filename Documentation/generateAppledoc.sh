08c9d905cc5d58da50d5fbe0365914c28f82e61c
Lars Anderson <lars.anderson@mutualmobile.com>
2013-06-20 09:36:43 -0500

    Deleting unused files

--- Documentation/generateAppledoc.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-echo "Generating Documentation for MMProgressHUD"
-appledoc --project-name MMProgressHUD --project-company "Mutual Mobile" --company-id com.mutualmobile --no-repeat-first-par ../Source
