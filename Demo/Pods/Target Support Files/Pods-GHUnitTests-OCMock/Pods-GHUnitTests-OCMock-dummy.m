03c8eaa939ebaa03e3961cb516a8bf893575e89e
Lars Anderson <ishka912@gmail.com>
2014-11-30 12:01:52 -0600

    Adding test files back to test targets only

---
+++ Demo/Pods/Target Support Files/Pods-GHUnitTests-OCMock/Pods-GHUnitTests-OCMock-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GHUnitTests_OCMock : NSObject
+@end
+@implementation PodsDummy_Pods_GHUnitTests_OCMock
+@end
---
+++ Demo/Pods/Target Support Files/Pods-LogicTests-MMProgressHUD/Pods-LogicTests-MMProgressHUD-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LogicTests_MMProgressHUD : NSObject
+@end
+@implementation PodsDummy_Pods_LogicTests_MMProgressHUD
+@end
